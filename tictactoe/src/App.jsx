7376c835e2b4de0ee74b2f4011e2145566740f7a
Ravi Savajiyani <rsavaj@gmail.com>
2025-02-11 12:18:31 -0700

    add basic layout

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,11 +1,27 @@
 // src/App.jsx
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  background-color: #f5f5f5;
+`
+
+const Title = styled.h1`
+  color: #333;
+  margin-bottom: 2rem;
+  font-size: 2rem;
+`
 
 function App() {
   return (
-    <div>
-      <h1>Tic Tac Toe</h1>
-    </div>
+    <Container>
+      <Title>Tic Tac Toe</Title>
+    </Container>
   )
 }
 
